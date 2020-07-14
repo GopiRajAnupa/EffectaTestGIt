9c4f9967da73dbf1bf113c8e68384231c08879e5
GopiRajAnupa <61490644+GopiRajAnupa@users.noreply.github.com>
2020-07-14 13:06:04 +0530

    SEL

--- pepapplication/EmployeesPEP11.java
+++ pepapplication/EmployeesPEP11.java
@@ -12,14 +12,13 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test; 
 import org.testng.annotations.AfterClass; 
 import org.openqa.selenium.Alert; 
-import org.openqa.selenium.JavascriptExecutor;gopiraj
+import org.openqa.selenium.JavascriptExecutor;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-gopiraj
 import java.util.Properties;
 
 
